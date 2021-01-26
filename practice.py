3361412531e1f9d883c2fd9200677e8fbf0d9536
songyouhyun <doong3373@gmail.com>
2021-01-26 22:01:55 +0900

    make cabinet

--- practice.py
+++ practice.py
@@ -1,19 +1,31 @@
-#) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+cabinet = {3:"유재석", 100:"김태호"}
+print(cabinet.get(5, "사용가능"))           #5번째에 있는 거를 가져오려다가 5번에 아무것도 없으면 그다음 value값을 가져옴
+print(3 in cabinet)
 
-# EX) http://naver.com
-# 규칙1 : http:// 부분은 제외 => naver.com
-# 규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
-# 규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+cabinet = {"13번 신발장":"유재석", "72번 신발장":"조세호"}
+print(cabinet.get("13번 신발장"))       #결과값 : 유재석
+print(cabinet.get("50번 신발장"))       #결과값 : none
 
-url = "http://naver.com"
+#새 손님
+print(cabinet)              #결과값 : {'A-20': '유재석', 'B-50':'조세호'}
+cabinet["13번 신발장"] = "김종국"      #원래 있던 유재석이 김종국으로 바뀜
+cabinet["54번 신발장"] = "박명수"     #박명수가 "C-100"에 추가
+print(cabinet)              #결과값 : {'A-20': '김종국', 'B-50': '조세호', 'C-100': '박명수'}
 
-pwd = url.replace("http://","")             #replace("원래 것","바뀔 것")
-# print(pwd)                                #결과값 : naver.com
+#빠진 손님
+del cabinet["13번 신발장"]         #김종국이 삭제됨
+print(cabinet)              #결과값 : {'B-50': '조세호', 'C-100': '박명수'}
 
-pwd = pwd[:pwd.index(".")]                  #처음부터 pwd의 .이 나오는 부분까지 슬라이싱
-# print(pwd)                                #결과값 : naver
+#지금 총 사용중인 고객 목록
+#key 값들만 출력
+print(cabinet.keys())       #결과값 : dict_keys(['B-50', 'C-100'])
 
-pwd = pwd[:3] + str(len(pwd)) + str(pwd.count("a")) + "!"
-#pwd를 처음부터 3번째글자까지 슬라이싱 + str(글자 갯수) + str(글자 내 'e' 갯수)) + "!"  
+#value 값들만 출력
+print(cabinet.values())
 
-print("{0}의 비밀번호는 {1} 입니다.".format(url,pwd))
+#key값과 value값 둘다 출력
+print(cabinet.items())
+
+#목욕탕 폐점
+cabinet.clear()
+print(cabinet)
