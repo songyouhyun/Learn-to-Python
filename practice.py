5b78508879051123cdffb0c9d55ed68170e5b1ca
songyouhyun <doong3373@gmail.com>
2021-01-26 23:30:59 +0900

    set

--- practice.py
+++ practice.py
@@ -1,16 +1,36 @@
-#Tuple
-menu = ("돈까스","치즈까스")
-print(menu[0])
-print(menu[1])
-#menu.add("생선까스")        #결과값 : Error / 이유 : Tuple은 값을 추가할 수 없음
+#세트 (set) = 집합
+# 중복 안됨, 순서가 없음
+my_set = {1,2,3,3}
+print(my_set)           #결과값 : {1,2,3}
 
 
-#일반적인 변수 출력방법
-name = "김종국"
-age = 20
-hobby = "코딩"
-print(name,age,hobby)       #결과값 : 김종국 20 코딩
+java_dev = {"유재석","김태호","양세형"}
+python_dev = set(["유재석","박명수"])   #set로 감싸주고 list를 정의, 값은 위와 동일
 
-#Tuple을 활용한 변수 출력방법
-(name, age, hobby) = ("김종국",20,"코딩")   #서로 다른 변수에 서로 다른 값들을 순서대로 넣어줄 수 있음
-print(name,age,hobby)                    #결과값 : 위와 동일
+#교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java_dev&python_dev)                      #결과값 : {'유재석'}
+print(java_dev.intersection(python_dev))        #결과값 :    ""
+
+#합집합 (java를 할 수 있거나, python을 할 수 있는 개발자)
+print(java_dev | python_dev)                    #결과값 : {'양세형', '박명수', '유재석', '김태호'}
+print(java_dev.union(python_dev))               #결과값 : {'양세형', '박명수', '유재석', '김태호'}
+                                                #하지만 결과값이 출력할때마다 다르게 나옴, 순서가 없기 때문
+
+#차집합 (java는 할 줄 알지만, python은 할 줄 모르는 개발자)
+print(java_dev - python_dev)                    #결과값 : {'양세형', '김태호'}
+print(java_dev.difference(python_dev))          #결과값 : {'양세형', '김태호'}
+#or (python은 할 줄 알지만, java는 할 줄 알지만, python은 할 줄 모르는 개발자)
+print(python_dev - java_dev)                    #결과값 : {'박명수'}
+print(python_dev.difference(java_dev))          #결과값 : {'박명수'}
+
+#python개발자 추가
+python_dev.add("김태호") 
+print(python_dev)                               #결과값 : {'박명수', '김태호', '유재석'}
+
+#java개발자 해고
+java_dev.remove("김태호")
+print(java_dev)                                 #결과값 : {'양세형', '유재석}
+
+
+
+############결과값 수정############
